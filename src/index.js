60e6c2f1aff035c2c7da9ec436ee756a4189e792
Samouil <samouilpratziotis@gmail.com>
2024-10-31 21:38:45 +0200

    minor fixes

--- src/index.js
+++ src/index.js
@@ -33,11 +33,11 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "about",
+        path: "/about",
         element: <About />,
       },
       {
-        path: "contact",
+        path: "/contact",
         element: <Contact />,
       },
     ],
